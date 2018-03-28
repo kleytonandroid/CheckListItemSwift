e28ed33bdfb53a5999dc2b401554387bec82ebb6
kleytonsantos <kleytonandroid@gmail.com>
2018-03-28 10:42:20 -0300

    First commit

---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Kleyton josé dos santos on 28/03/18.
+//  Copyright © 2018 KleytonCorporation. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 100
+        
+    }
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "ChecklistItem",
+                for: indexPath)
+            
+            let label = cell.viewWithTag(1000) as! UILabel
+            if indexPath.row % 5 == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row % 5 == 1 {
+                label.text = "Brush my teeth"
+            } else if indexPath.row % 5 == 2 {
+                label.text = "Learn iOS development"
+            } else if indexPath.row % 5 == 3 {
+                label.text = "Soccer practice"
+            } else if indexPath.row % 5 == 4 {
+                label.text = "Eat ice cream"
+            }
+            // End of new code block
+            return cell
+            
+    }
+
+
+}
+
+
