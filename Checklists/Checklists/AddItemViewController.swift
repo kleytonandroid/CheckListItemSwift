4f97a55465098664a179ef1ce775d9f770a2b8ed
kleytonsantos <kleytonandroid@gmail.com>
2018-03-29 08:37:01 -0300

    Customize the textfield on AddNewItemScreen pag - 274

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+            return nil
+    }
+    
 }
