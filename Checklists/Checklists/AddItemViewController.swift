bd6c91b6eaf57e6c43bf5961a85f5fcfe79eaf49
kleytonsantos <kleytonandroid@gmail.com>
2018-03-29 12:35:50 -0300

    Final chapter 12

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class AddItemViewController: UITableViewController {
     
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,5 +35,23 @@ class AddItemViewController: UITableViewController {
                             willSelectRowAt indexPath: IndexPath) -> IndexPath? {
             return nil
     }
+ 
     
+}
+
+extension AddItemViewController: UITextFieldDelegate{
+    
+    func textField(_ textField: UITextField,
+                        shouldChangeCharactersIn range: NSRange,
+                        replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,with: string)
+        if newText.isEmpty {
+                doneBarButton.isEnabled = false
+            } else {
+                doneBarButton.isEnabled = true
+            }
+            return true
+    }
 }
